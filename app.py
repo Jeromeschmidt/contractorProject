1aaefdc2660609e13918126c00255b144af73b3f
Jerome Schmidt <schmidt.jerome@yahoo.com>
2019-10-04 19:13:13 -0700

    owner can edit and delete items easier

--- app.py
+++ app.py
@@ -37,20 +37,32 @@ def item_submit():
         'in_shopping_cart': False
     }
     item_id = items.insert_one(item).inserted_id
-    return render_template('owner_view_item.html', item=item)
+    return render_template('owner.html', items=items.find())
 
 @app.route('/owner/add_item')
-def item():
+def add_item():
     """Create a new item."""
     return render_template('item_new.html', item={}, title='New item')
 
-@app.route('/owner/<item_id>/edit')
+@app.route('/owner/<item_id>/edit', methods=['POST'])
 def edit_item(item_id):
     """edit an item."""
     item = items.find_one({'_id': ObjectId(item_id)})
     return render_template('edit_item.html', item=item, title='edit item')
 
 @app.route('/owner/<item_id>', methods=['POST'])
+def item_update(item_id):
+    """Submit an edited playlist."""
+    updated_item = {
+        'title': request.form.get('title'),
+        'price': request.form.get('price'),
+    }
+    items.update_one(
+        {'_id': ObjectId(item_id)},
+        {'$set': updated_item})
+    return redirect(url_for('owner', item_id=item_id))
+
+@app.route('/owner/<item_id>/delete', methods=['POST'])
 def item_delete(item_id):
     """Delete one item."""
     items.delete_one({'_id': ObjectId(item_id)})
