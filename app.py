37a57bbe476aa3d606dbb31285eb0f3ca4e7b65b
Jerome Schmidt <schmidt.jerome@yahoo.com>
2019-10-08 20:57:21 -0700

    trying to get heroku to work

--- app.py
+++ app.py
@@ -164,4 +164,4 @@ def thanks():
     return render_template('thanks.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
