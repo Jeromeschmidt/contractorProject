92f10241744647d050c05c6086f3f71e3dfdbecb
Jerome Schmidt <schmidt.jerome@yahoo.com>
2019-10-08 20:30:27 -0700

    added image support and some inventory

--- app.py
+++ app.py
@@ -32,82 +32,95 @@ def store_index():
     """Show all items."""
     return render_template('store_index.html', items=items.find())
 
+#owner landing page
 @app.route('/owner')
 def owner():
     """takes user to owner landing page"""
     return render_template('owner.html', items=items.find())
 
+#displays a single item to owner
 @app.route('/owner/<item_id>')
 def owner_view_item(item_id):
     """Show a single item."""
     item = items.find_one({'_id': ObjectId(item_id)})
     return render_template('owner_view_item.html', item=item)
 
+#creates a new item
 @app.route('/item', methods=['POST'])
 def item_submit():
     """Submit a new item."""
     item = {
         'title': request.form.get('title'),
         'price': request.form.get('price'),
+        'images': request.form.get('images').split(),
         'created_at': datetime.now(),
         'in_shopping_cart': False
     }
     item_id = items.insert_one(item).inserted_id
     return render_template('owner.html', items=items.find())
 
+#adds a new item
 @app.route('/owner/add_item')
 def add_item():
     """Create a new item."""
     return render_template('item_new.html', item={}, title='New item')
 
+#lets the owner edit an item
 @app.route('/owner/<item_id>/edit', methods=['POST'])
 def edit_item(item_id):
     """edit an item."""
     item = items.find_one({'_id': ObjectId(item_id)})
     return render_template('edit_item.html', item=item, title='edit item')
 
+#updates an edited item
 @app.route('/owner/<item_id>', methods=['POST'])
 def item_update(item_id):
     """Submit an edited playlist."""
     updated_item = {
         'title': request.form.get('title'),
         'price': request.form.get('price'),
+        'images': request.form.get('images').split()
     }
     items.update_one(
         {'_id': ObjectId(item_id)},
         {'$set': updated_item})
     return redirect(url_for('owner', item_id=item_id))
 
+#lets owner delete an item
 @app.route('/owner/<item_id>/delete', methods=['POST'])
 def item_delete(item_id):
     """Delete one item."""
     items.delete_one({'_id': ObjectId(item_id)})
     return redirect(url_for('owner'))
 
-#specific item
+#displays a specific item
 @app.route('/<item_id>')
 def item_show(item_id):
     """Show a single item."""
     item = items.find_one({'_id': ObjectId(item_id)})
     return render_template('item_show.html', item=item)
 
+#shows the users shopping cart
 @app.route('/shopping_cart')
 def shopping_cart():
     """display user's shopping cart"""
     return render_template('shopping_cart.html', items=items.find())
 
+#adds an item to the shopping cart
 @app.route('/shopping_cart/<item_id>/add_to_cart', methods=['POST'])
 def add_to_shopping_cart(item_id):
     """display user's shopping cart"""
     items.update_one({'_id':ObjectId(item_id)}, {"$set": {"in_shopping_cart" : True}}, upsert=False)
     return render_template('shopping_cart.html', items=items.find())
 
+#deletes an item from the shopping cart
 @app.route('/shopping_cart/<item_id>/delete_from_cart', methods=['POST'])
 def delete_from_shopping_cart(item_id):
     """display user's shopping cart"""
     items.update_one({'_id':ObjectId(item_id)}, {"$set": {"in_shopping_cart" : False}}, upsert=False)
     return render_template('shopping_cart.html', items=items.find())
 
+#finds the total amount due for items in shopping cart
 def amount_in_cart():
     amount = 0
     for item in items.find():
@@ -116,11 +129,13 @@ def amount_in_cart():
     amount = amount*100
     return amount
 
+#takes the user to a checkout page
 @app.route('/shopping_cart/checkout')
 def checkout():
     """display user's shopping cart"""
     return render_template('checkout.html', key=stripe_pub_key, amount=amount_in_cart())
 
+#takes the user to a thank you page, and sends a  text message to owner
 @app.route('/shopping_cart/checkout/thanks', methods=['POST'])
 def thanks():
     #processes stripe payment
